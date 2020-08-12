4d05d46800b3e78ffe49a3b4bec653947cf5164b
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-08-12 10:08:39 +0100

    Commonise Circe and STTP versions to simplify dependencies file

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,15 +1,18 @@
 import sbt._
 
 object Dependencies {
+  lazy val circeVersion = "0.13.0"
+  lazy val sttpVersion = "2.2.4"
+
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.0-M2"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock-jre8" % "2.26.0"
-  lazy val circeCore = "io.circe" %% "circe-core" % "0.13.0"
-  lazy val circeParser = "io.circe" %% "circe-parser" % "0.13.0"
-  lazy val circeGeneric = "io.circe" %% "circe-generic" % "0.13.0"
-  lazy val circeGenericExtras = "io.circe" %% "circe-generic-extras" % "0.13.0"
-  lazy val sttp = "com.softwaremill.sttp.client" %% "core" % "2.2.4"
-  lazy val sttpCirce = "com.softwaremill.sttp.client" %% "circe" % "2.2.4"
-  lazy val sttpAsyncClient = "com.softwaremill.sttp.client" %% "async-http-client-backend-future" % "2.2.4"
+  lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
+  lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
+  lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
+  lazy val circeGenericExtras = "io.circe" %% "circe-generic-extras" % circeVersion
+  lazy val sttp = "com.softwaremill.sttp.client" %% "core" % sttpVersion
+  lazy val sttpCirce = "com.softwaremill.sttp.client" %% "circe" % sttpVersion
+  lazy val sttpAsyncClient = "com.softwaremill.sttp.client" %% "async-http-client-backend-future" % sttpVersion
   lazy val oauth2 = "com.nimbusds" % "oauth2-oidc-sdk" % "6.23"
   lazy val sangria = "org.sangria-graphql" %% "sangria" % "2.0.0-M3"
 }
