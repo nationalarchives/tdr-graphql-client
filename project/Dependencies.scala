94cb79dd56cf374679b50a8534db4baff98f6498
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-03-30 08:35:25 +0000

    Update sangria to 2.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,5 +14,5 @@ object Dependencies {
   lazy val sttpCirce = "com.softwaremill.sttp.client3" %% "circe" % sttpVersion
   lazy val sttpAsyncClient = "com.softwaremill.sttp.client3" %% "async-http-client-backend-future" % sttpVersion
   lazy val oauth2 = "com.nimbusds" % "oauth2-oidc-sdk" % "6.23"
-  lazy val sangria = "org.sangria-graphql" %% "sangria" % "2.0.0-M3"
+  lazy val sangria = "org.sangria-graphql" %% "sangria" % "2.0.1"
 }
