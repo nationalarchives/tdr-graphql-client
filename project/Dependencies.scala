643902ca658ed8c158b17c5bc7245b838912d754
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-05-07 08:23:41 +0000

    Update client3:async-http-client-backend-future, ... to 3.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val circeVersion = "0.14.1"
-  lazy val sttpVersion = "3.5.2"
+  lazy val sttpVersion = "3.6.1"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock-jre8" % "2.33.2"
