06955121294a52020a70ab222dfb9a0b34e0f5ec
TomJKing <tom.king@nationalarchives.gov.uk>
2022-09-20 09:02:34 +0100

    Circe generic extras has no version '14.0.3'

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val circeGenericExtrasVersion = "0.14.3"
+  lazy val circeGenericExtrasVersion = "0.14.2"
   lazy val circeVersion = "0.14.3"
   lazy val sttpVersion = "3.8.0"
 
