26de6b5e19a009f646d5f443d3c2d8298a3df79e
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-10-01 08:27:09 +0000

    Update client3:circe, client3:core to 3.8.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val circeGenericExtrasVersion = "0.14.2"
   lazy val circeVersion = "0.14.3"
-  lazy val sttpVersion = "3.8.0"
+  lazy val sttpVersion = "3.8.2"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.13"
   lazy val wiremock = "com.github.tomakehurst" % "wiremock-jre8" % "2.34.0"
